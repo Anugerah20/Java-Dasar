8682a592d197f27b7caaade6a34cdcc7706eb79a
Nabil <nabilpangestu123@gmail.com>
2021-03-07 21:12:35 +0700

    Add files via upload

---
+++ Java Variable/StringVariable2.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)StringVariable2.java
+ *
+ *
+ * @author 
+ * @version 1.00 2021/3/4
+ */
+
+
+public class StringVariable2 {
+	public static void main (String[] args) {
+		int Num = 73;
+		System.out.println (Num);
+    }
+    
+    
+}
