e863775a04fda6fcaed5904ec185e0bc92d62169
Nabil <nabilpangestu123@gmail.com>
2021-03-07 21:22:23 +0700

    Update JavaHello.java

--- Intro Java/JavaHello.java
+++ Intro Java/JavaHello.java
@@ -10,7 +10,7 @@
 public class JavaHello {
 
     	public static void main(String[] args) {
-    		String satu = "Hello";
+    		String satu = "Hello ";
     		String dua = "Java";
     		String text = satu + dua;
     		System.out.println (text);
