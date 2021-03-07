37e3ebc9beb3c274cce70a2cde59ead6b0e12b1b
Nabil <nabilpangestu123@gmail.com>
2021-03-07 21:04:24 +0700

    Add files via upload

---
+++ Java Variable/StringVariable.java
@@ -0,0 +1,18 @@
+/**
+ * @(#)String.java
+ *
+ *
+ * @author Abi
+ * @version 1.00 2021/3/4
+ */
+
+
+public class StringVariable {
+
+   public static void main (String[] args) {
+   	
+   	String name = "Nabil Anugerah";
+   	System.out.println (name);
+   }
+    
+}
---
+++ Java Variable/StringVariable2.java
@@ -0,0 +1,13 @@
+/**
+ * @(#)StringVariable2.java
+ *
+ *
+ * @author 
+ * @version 1.00 2021/3/4
+ */
+
+
+public enum StringVariable2 {
+    
+    
+}
