745dc438111fbdcde07b9dbd6378509b150e0bcd
Nabil <nabilpangestu123@gmail.com>
2021-03-07 21:17:06 +0700

    Add files via upload

---
+++ Intro Java/JavaHello.java
@@ -0,0 +1,18 @@
+/**
+ * @(#)JavaHello.java
+ *
+ *
+ * @author 
+ * @version 1.00 2021/3/1
+ */
+
+
+public class JavaHello {
+
+    	public static void main(String[] args) {
+    		String nama = "Nabil Anugerah Pangestu";
+    		String tanya = "Hello semuanya, Perkenalkan nama saya " + nama + ". Saya adalah seorang Front-End Developer Di perusahaan Anugerah.";
+    		String text = tanya + nama;
+    		System.out.println (text);
+	    }  
+}
