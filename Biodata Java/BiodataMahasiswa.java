668a4ff345e26298823e15920f69b9fd106bbd1d
Nabil <nabilpangestu123@gmail.com>
2021-03-07 20:56:42 +0700

    Add files via upload

---
+++ Biodata Java/BiodataMahasiswa.java
@@ -0,0 +1,26 @@
+/**
+ * @(#)AppMahasiswa.java
+ *
+ *
+ * @author Abi
+ * @version 1.00 2021/3/3
+ */
+
+
+public class BiodataMahasiswa {
+	public static void main (String[] args) {
+		String nama = "Angga";
+		String nim = "123456789";
+		String fak = "Teknologi Informatika";
+		String jur = "Cyber Security";
+		
+		System.out.println ("Biodata Mahasiswa Universitas ABC");
+		System.out.println ("=====================================");
+		System.out.println ("Nama \t: " + nama);
+		System.out.println ("Nim \t: " + nim);
+		System.out.println ("Fak \t: " + fak);
+		System.out.println ("Jur \t: " + jur);
+		System.out.println ("=====================================");
+	    }
+    }
+    
